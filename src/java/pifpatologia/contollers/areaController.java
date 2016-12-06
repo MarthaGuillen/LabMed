54e4ad22c2f04aca51a016f9560a30354597a323
SIST001 <SIST001@SIST001-PC>
2016-12-06 09:10:45 -0600

    Merge origin/master
    Conflicts:
    	web/WEB-INF/jsp/principal.jsp

---
+++ src/java/pifpatologia/contollers/areaController.java
@@ -0,0 +1,91 @@
+
+package pifpatologia.contollers;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import modelo.dao.AreaDAO;
+import modelo.dao.PerfilesDAO;
+import modelo.dao.UsuarioDAO;
+import modelo.dao.UsuarioPorPerfilDAO;
+import modelo.pojo.Area;
+import modelo.pojo.Perfiles;
+import modelo.pojo.Usuarios;
+import modelo.util.EnviarCorreo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ *
+ * @author Sinergia14
+ */
+@Controller
+public class areaController {
+       
+    @RequestMapping("/NuevaArea.htm")
+    public String NuevaArea(){        
+        return "Parametria/NuevaArea";
+    }
+    
+    @RequestMapping(value="/NuevaArea.htm", method = RequestMethod.POST)
+    public String InsertNuevaArea(Model m, HttpServletRequest request, @RequestParam("NombreArea") String nombre, @RequestParam("Estado") String estado){
+        AreaDAO area = new AreaDAO();
+        boolean estadoU=true;
+        
+        if(estado.equals("Inactivo"))
+            estadoU=false;
+        
+        area.AgregarArea(nombre, estadoU, 0, "");
+        return "principal";
+    }
+    
+    @RequestMapping("/ListadoDeAreas.htm")
+    public String ListaAreas(Model m, HttpServletRequest request){
+        
+        AreaDAO area = new AreaDAO();        
+        ArrayList<Area> listaArea = area.ObtenerTodasAreas();
+        m.addAttribute("listaArea", listaArea);
+        return "Parametria/ListaAreas";
+        
+    }
+       
+//@RequestMapping(value = "upAreaEstado.htm", method = RequestMethod.POST) 
+//    public ModelAndView upAreaEst(@RequestParam("bol") String bol,@RequestParam("idp") int idp) throws Exception {
+//        ModelAndView mv = new ModelAndView("cargatempPermisos");
+//        AreaDAO opc = new AreaDAO();
+//
+//        String idper = opc.updAreaestado(bol,idp);       
+//
+//        mv.addObject("resp","No");
+//        return mv;
+//    } 
+    
+   
+      
+     
+    @RequestMapping(value = "CargaVistaArea.htm", method = RequestMethod.POST) 
+     public ModelAndView guarda(@RequestParam("idp") int idp) throws Exception {
+        ModelAndView mv = new ModelAndView("Parametria/pgCargaVistaArea");
+        AreaDAO opc = new AreaDAO();
+        List listaP1 = opc.ObtenerArea(idp);
+        System.out.println(idp);
+        String nombre = "";
+        String estado = "";
+        List<Object[]> listDatosp = listaP1;
+        for (Object[] datos : listDatosp) {
+            nombre = (String) datos[0].toString();
+            estado = (String) datos[1].toString();
+         }
+        mv.addObject("Nombre",nombre);
+        mv.addObject("estado",estado);
+       
+      return mv;
+    }
+    
+}
